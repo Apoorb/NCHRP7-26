03d6598ecf30f374301cec1371090a17cd7624ec
Apoorb <apoorb2510@gmail.com>
2020-10-26 17:41:37 -0400

    Add Azy's code for capacity estimation and plotting.

--- src/data/scratch/output_pre_breakdown_flow_speed.py
+++ src/data/scratch/output_pre_breakdown_flow_speed.py
@@ -71,7 +71,7 @@ def read_ramp_data(file_path, time, is_flow_volume):
             raw_data.duplicated(["Timestamp", "Lane"], keep=False)
         ]
         raw_data = raw_data[~raw_data.duplicated(["Timestamp", "Lane"], keep="first")]
-    raw_data_ramp = raw_data.groupby("Timestamp").Volume.mean()
+    raw_data_ramp = raw_data.groupby("Timestamp").Volume.sum()
     raw_data_ramp.index = pd.to_datetime(raw_data_ramp.index)
     volumes = raw_data_ramp.values.T.tolist()
     measurement_duration = list(raw_data_ramp.index.to_pydatetime())
@@ -351,6 +351,308 @@ def get_weave_ramp_file_dict(path_to_weave_ramp_data_):
     return ramp_files_dict_
 
 
+def pre_post_breakdown_flowrate_fn(pre_post_breakdown_index, speed, volume):
+    pre_post_breakdown_flowrate = []
+    pre_post_breakdown_speed = []
+    avg_pre_post_breakdown_flowrate = 0
+    temp = 0
+    for i in range(len(pre_post_breakdown_index)):
+        pre_post_breakdown_flowrate.append(volume[pre_post_breakdown_index[i]])
+        pre_post_breakdown_speed.append(speed[pre_post_breakdown_index[i]])
+        avg_pre_post_breakdown_flowrate = avg_pre_post_breakdown_flowrate + (
+            float(volume[pre_post_breakdown_index[i]])
+        )
+        temp = i
+    avg_pre_post_breakdown_flowrate = avg_pre_post_breakdown_flowrate / temp - 1
+    return (
+        pre_post_breakdown_flowrate,
+        pre_post_breakdown_speed,
+        avg_pre_post_breakdown_flowrate,
+    )
+
+
+def counts_ranges(volume_data, flow_seq):
+    counts = []
+    for i in range(1, len(flow_seq)):
+        temp = 0
+        for j in range(len(volume_data)):
+            if volume_data[j] >= flow_seq[i - 1] and volume_data[j] < flow_seq[i]:
+                temp = temp + 1
+        counts.append(temp)
+    # print(counts)
+    return counts
+
+
+def avg_flowrate_fn(volume_data, flow_seq):
+    sum = []
+    avg = []
+    for i in range(1, len(flow_seq)):
+        temp = 0
+        temp2 = 0
+        for j in range(1, len(volume_data)):
+            # print(j)
+            if volume_data[j] >= flow_seq[i - 1] and volume_data[j] < flow_seq[i]:
+                # print(volume_data[j])
+                sum.append(volume_data[j])
+        if not sum:
+            avg.append(0)
+        else:
+            avg.append(round(np.mean(sum)))
+        sum = []
+    return avg
+
+
+def cumm_breakdown_prob(counts):
+    cumm_pre_breakdown_distr = []
+    cumm_pre_breakdown = []
+
+    cumm_pre_breakdown.append(0)
+    for i in range(len(counts)):
+        cumm_pre_breakdown.append((cumm_pre_breakdown[i] + counts[i]))
+    for i in range(len(cumm_pre_breakdown)):
+        cumm_pre_breakdown_distr.append(
+            round(cumm_pre_breakdown[i] / float(np.sum(counts)) * 100, 1)
+        )
+        # print(cumm_pre_breakdown_distr[i])
+    return (cumm_pre_breakdown, cumm_pre_breakdown_distr)
+
+
+def breakdown_prob(pre_breakdown_counts, uncong_counts, flow_seq):
+    breakdown_probability = []
+    breakdow_flow_seq = []
+    # breakdown_probability.append(0)
+    for i in range(len(pre_breakdown_counts)):
+        if uncong_counts[i] != 0:
+
+            breakdown_probability.append(
+                round(float(pre_breakdown_counts[i] / float(uncong_counts[i])) * 100, 1)
+            )
+            breakdow_flow_seq.append(flow_seq[i + 1])
+        # else:
+        #     breakdown_probability.append(0)
+
+    # print(breakdown_probability)
+    return (breakdown_probability, breakdow_flow_seq)
+
+
+def detector_info(x, file):
+    file_name = file.split(".csv")[0]
+    zone_number = str(file_name).split("Lane_Readings_")[1].split("-")[0]
+    lane_number = str(file_name).split("Lane_Readings_")[1].split("-")[1]
+    lane_id = str(file_name).split("Lane_Readings_")[1].split("-")[2]
+    if x == 0:
+        print("Lane Number: " + str(lane_number))
+        return str(zone_number) + "," + str(lane_id) + "," + str(lane_number)
+    else:
+
+        return int(lane_number)
+
+
+def weibull_fn(flow_seq, breakdown_probability):
+    # print(flow_seq[1:])
+    x = np.zeros(len(breakdown_probability))
+    # print(x)
+    for i in range(len(breakdown_probability)):
+        if breakdown_probability[i] > 0 and breakdown_probability[i] < 100:
+            x[i] = 1
+    # print(x)
+    start_index = []
+    end_index = []
+    temp = 1
+    for i in range(1, len(x)):
+        if x[i] == temp:
+            if temp == 1:
+                start_index.append(i)
+                temp = 0
+            else:
+                end_index.append(i - 1)
+                temp = 1
+    if len(start_index) != len(end_index):
+        end_index.append(len(x) - 1)
+    # print(start_index)
+    # print(end_index)
+    max1 = np.subtract(end_index, start_index)
+    index = np.argmax(max1)
+    # print(start_index[int(index)])
+    # print(end_index[int(index)])
+
+    X = []
+    Y = []
+
+    for i in range(start_index[int(index)], end_index[int(index)] + 1):
+        X.append(np.log(flow_seq[i]))
+        Y.append(np.log(-np.log(1 - breakdown_probability[i] / 100)))
+    slope, intercept, r_val, p_val, std_err = linregress(X, Y)
+    beta = slope
+    alpha = np.exp(intercept / (-beta))
+    # print(str(alpha) + " " + str(beta))
+    weibull_x = np.linspace(100, 3000, 2000)
+    fit_y = intercept + slope * np.log(weibull_x)
+    weibull_vals = 1 - np.exp(-np.exp(fit_y))
+
+    return (weibull_vals, weibull_x, alpha, beta)
+
+
+def estimated_capacity_fn(alpha, beta):
+    estimated_capacity = alpha * 0.163 ** (1 / beta)
+    return round(estimated_capacity)
+
+
+def output_file(
+    database,
+    date_time,
+    FFS,
+    volume,
+    pre_breakdown_counts,
+    estimated_capacity,
+    file,
+    alpha,
+    beta,
+    lane,
+):
+    filename = "Data_output.csv"
+    if os.path.isfile(filename):
+        with open("Data_output.csv", "a") as f:
+            f.write(
+                database
+                + ","
+                + str(os.path.basename(file))
+                + ","
+                + str(lane)
+                + ","
+                + str(date_time[0])
+                + ","
+                + str(date_time[len(date_time) - 1])
+                + ","
+                + str(FFS)
+                + ","
+                + str(len(volume))
+                + ","
+                + str(sum(pre_breakdown_counts))
+                + ","
+                + str(alpha)
+                + ","
+                + str(beta)
+                + ","
+                + str(estimated_capacity)
+                + ","
+                + "\n"
+            )
+
+    else:
+        with open("Data_output.csv", "a") as f:
+            f.write(
+                "Database,File,Lane Number,Start Date, End Date, FFS, Total Counts, Breakdown Events, Alpha, Beta, Estimated Capacity\n"
+            )
+            f.close()
+        output_file(
+            database,
+            date_time,
+            FFS,
+            volume,
+            pre_breakdown_counts,
+            estimated_capacity,
+            file,
+            alpha,
+            beta,
+            lane,
+        )
+
+
+def plot_figure_1(
+    FFS,
+    pre_breakdown_counts,
+    volume,
+    speed,
+    pre_breakdown_speed,
+    pre_breakdown_flowrate,
+    estimated_capacity,
+    file,
+    lane,
+    alpha,
+    beta,
+):
+    global annot, fig, ax, sc
+    file_name = file.split(".xlsx")[0]
+    plt.figure()
+    plt.scatter(
+        [volume],
+        [speed],
+        label="Speed-Flow Curve",
+        color="cornflowerblue",
+        marker=".",
+        s=12,
+    )
+    sc = plt.scatter(
+        [pre_breakdown_flowrate],
+        [pre_breakdown_speed],
+        label="Prebreakdown Events",
+        color="red",
+        marker="x",
+        s=12,
+    )
+    # ax1.scatter([pre_breakdown_flowrate], [pre_breakdown_speed], label="PreBreakdown_speed vs PreBreakdown_volume",color="blue")
+    # ax2.plot(flow_seq, cumm_pre_breakdown_distr, label='Observed Pre-Breakdown Probability Distr.', color='blue')
+    # ax2.scatter([flow_seq], [cumm_pre_breakdown_distr], color='blue')
+    # plt.title('FFS: %d MPH , Breakdowns: %d, Samples: %d, Alpha: %d, Beta: %s,  Estimated Capacity: %d' % (FFS,sum(pre_breakdown_counts), len(volume),alpha,str(beta),estimated_capacity), fontsize=12)
+    plt.legend(loc="best", fontsize=10)
+    # ax2.legend(loc="upper right", fontsize=10)
+    plt.xlabel("Flow Rate (veh/hr/ln)", fontsize=12)
+    plt.ylabel("Speed (mph)", fontsize=12)
+    # ax2.set_ylabel("Probability of Breakdown", fontsize=12)
+    # plt.xlim([0,max(uncong_volume)+200])
+    figure = plt.gcf()  # get current figure
+    figure.set_size_inches(5.5, 3)
+    plt.savefig(str(file_name) + "_" + str(lane) + "_time_1.png", bbox_inches="tight")
+
+
+def plot_figure_2(
+    FFS,
+    pre_breakdown_counts,
+    volume,
+    flow_seq,
+    cumm_pre_breakdown_distr,
+    weibull_vals,
+    weibull_x,
+    estimated_capacity,
+    file,
+    lane,
+    alpha,
+    beta,
+):
+    file_name = file.split(".xlsx")[0]
+    plt.figure()
+    plt.plot(
+        flow_seq,
+        cumm_pre_breakdown_distr,
+        label="Observed Prebreakdown CDF",
+        color="red",
+        linestyle="--",
+    )
+    # plt.scatter([flow_seq], [cumm_pre_breakdown_distr], color='red')
+    plt.plot(
+        weibull_x, weibull_vals, label="Fitted Weibull CDF", color="cornflowerblue"
+    )
+    # plt.scatter([flow_seq], [weibull_vals], label='Weibull CDF', color='cornflowerblue')
+    # ax1.plot(flow_seq, breakdown_probability, label='Probability of Breakdown', color='green')
+    # ax1.scatter([flow_seq], [breakdown_probability], color='green')
+    # plt.title('FFS: %d MPH , Lane: %d, Breakdowns: %d, Samples: %d, Alpha: %d, Beta: %s,  Estimated Capacity: %d' % (FFS, lane, sum(pre_breakdown_counts), len(volume),alpha,str(beta),estimated_capacity), fontsize=12)
+    plt.legend(loc="best", fontsize=10)
+    plt.xlabel("Flow Rate (veh/hr/ln)", fontsize=12)
+    plt.ylabel("Probability of Breakdown", fontsize=12)
+    # plt.xlim(flow_seq)
+    plt.ylim([0, 1.1])
+    figure = plt.gcf()  # get current figure
+    figure.set_size_inches(5.5, 3)
+    plt.savefig(str(file_name) + "_" + str(lane) + "_time_2.png", bbox_inches="tight")
+
+
+def show_figures(start):
+    print("Code run time: " + str(datetime.datetime.utcnow() - start))
+    # plt.show()
+
+
+
 if __name__ == "__main__":
     path_to_site_summary = (
         r"C:\Users\abibeka\Kittelson & Associates, Inc\Burak Cesme -"
@@ -458,12 +760,21 @@ if __name__ == "__main__":
             ramp_uncong,
             ramp_bd,
         ) = breakdown_fn(speed_1, volume_1, ramp_volume_1, FFS, 4)
-
-        # Step 3
+        uncong_counts = counts_ranges(uncong_volume, flow_seq)
+        # Step 4
+        avg_flowrate = avg_flowrate_fn(uncong_volume, flow_seq)
+        # Step 5
         pre_breakdown_index, post_breakdown_index = pre_post_breakdown_index_fn(
-            breakdown_index, 0
-        )
-        # step 4
+            breakdown_index, 0)
+        (
+            pre_breakdown_flowrate,
+            pre_breakdown_speed,
+            avg_pre_breakdown_flowrate,
+        ) = pre_post_breakdown_flowrate_fn(pre_breakdown_index, speed, volume)
+        pre_breakdown_counts = counts_ranges(pre_breakdown_flowrate, flow_seq)
+        print((pre_breakdown_counts))
+
+        # step x
         file_comp = file.split(".")
         file_comp[0] = file_comp[0] + "_pre_brkdn"
         out_file = ".".join(file_comp)
@@ -476,5 +787,35 @@ if __name__ == "__main__":
             pre_breakdown_index,
             out_file_path,
         )
+        # Step 6
+        breakdown_probability, breakdown_flow_seq = breakdown_prob(
+            pre_breakdown_counts, uncong_counts, flow_seq
+        )
+        print(breakdown_probability)
+        # Step 7
+        weibull_vals, weibull_x, alpha, beta = weibull_fn(
+            avg_flowrate, breakdown_probability
+        )
+        print("Alpha: " + str(round(alpha)) + " Beta: " + str(round(beta, 1)))
+        #     # print((weibull_vals))
+
+        # Step 8
+        estimated_capacity = estimated_capacity_fn(alpha, beta)
+        print("Estimated Capacity: " + str(estimated_capacity))
+        # Step 7
+        output_file(
+            file,
+            date_time,
+            round(FFS, 0),
+            volume,
+            pre_breakdown_counts,
+            estimated_capacity,
+            file_path,
+            round(alpha, 0),
+            round(beta, 1),
+            0,
+        )
+
+        #
 
     # show_figures(start)
